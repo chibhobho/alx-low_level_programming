17a93ef27c38c1eb52afa451a84cb199aa07b9eb
chibhobho <tmuseg@gmail.com>
2022-09-10 09:29:26 -0700

    task1

---
+++ 1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- Entry point
+ *
+ * Return: sign of numbers
+ */
+int main(void)
+{
+int n;
+int e;
+srand(time(0));
+n = rand() RAND_MAX / 2;
+e = n % 10;
+if (e > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, e);	
+}
+else if (e == 0)
+{
+printf(Last digit of %d is %d and is 0\n", n, e);
+}
+else if ((e < 6) && (e!= 0))
+{
+printf(Last digit of %d is %d and is less than 6 and not 0\n", n, e);
+}
+return (0);
+}
