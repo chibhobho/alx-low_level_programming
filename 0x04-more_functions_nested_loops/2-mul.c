fab31d7df256f17db3a472b242b8eb2b3c3ce9dd
chibhobho <tmuseg@gmail.com>
2022-09-18 12:46:41 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul- Multiply numbers
+ * @a: Number to b evaluated
+ * @b: Number to b evaluated
+ *
+ * Return: Always 1 on success
+ */
+int mul(int a, int b)
+{
+int answer;
+answer = a * b;
+return (answer);
+}	
