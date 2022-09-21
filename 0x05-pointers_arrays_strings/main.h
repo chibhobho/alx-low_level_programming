fa4249e5aa2204c36135e6e1f6eb54d82cc9604e
chibhobho <tmuseg@gmail.com>
2022-09-21 02:31:25 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a point and update the value
+ * @n: the pointer
+ * Return: no return
+ */
+
+void reset_to_98(int *n)
+
+{
+*n = 98;
+}
